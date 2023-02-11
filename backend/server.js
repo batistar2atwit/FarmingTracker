e6280c835b32519ece78287b5782d16183b2936e
KiosoKen <randy032003@gmail.com>
2023-02-11 06:20:05 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/users');
 
 // express app
 const app = express();
