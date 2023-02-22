d237d81924bd63c44223431b9e1bd523d73e74ae
KiosoKen <randy032003@gmail.com>
2023-02-22 07:02:52 -0500

    Added files for BossMats tab

---
+++ frontend/src/components/BossMaterials/CharAscMats.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+const CharAscMats = () => {
+
+   const
+
+
+
+   return <div>
+      <table>
+         <thead>
+            <th></th>
+            <th></th>
+            <th></th>
+         </thead>
+         <tbody>
+            <tr>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+            </tr>
+            <tr>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+            </tr>
+            <tr>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+            </tr>
+         </tbody>
+      </table>
+   </div>
+}
